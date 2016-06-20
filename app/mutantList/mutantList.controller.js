344b1125022a748007c760432d28ea49986ea6c9
tworns <tworns@yahoo.com>
2016-06-20 11:37:43 -0400

    Added mutant contstructor, bound form values to new mutant in ng-model

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -5,5 +5,19 @@
   function MutantListController(){
     var vm = this;
     vm.mutants = ['Wolverine', 'X23', 'Dave'];
+    vm.addMutant = addMutant;
+    //vm.newMutant = newMutant;
+    function addMutant() {
+      vm.mutants.push(vm.newMutant.name);
+    }
+    function Mutant() {
+      this.name = '';
+      this.phone ='';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
+    function newMutant() {
+  }
   }
 })();
