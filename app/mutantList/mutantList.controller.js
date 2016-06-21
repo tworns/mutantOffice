11049b104985dae16e96bd062a2afe04f4f25d09
tworns <tworns@yahoo.com>
2016-06-21 10:22:00 -0400

    Text button now adds a text object to the database, changes mutant complete variable

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -5,10 +5,12 @@
   function MutantListController($firebaseArray){
     var vm = this;
     var mutantsRef = firebase.database().ref().child('mutants');
+    var textRef = firebase.database().ref().child('texts');
     vm.mutants = $firebaseArray(mutantsRef);
     vm.addMutant = addMutant;
     vm.toggleComplete = toggleComplete;
     vm.deleteMutant = deleteMutant;
+    vm.sendText = sendText;
 
     function addMutant() {
       vm.mutants.$add(vm.newMutant);
@@ -25,6 +27,16 @@
   }
     function deleteMutant(mutant){
       vm.mutants.$remove(mutant);
+    }
+    function sendText(mutant){
+      var newText = {
+        topic: mutant.topic,
+        name: mutant.name,
+        phone: mutant.phone
+      };
+      textRef.push(newText);
+      vm.mutants.$save(mutant);
+
     }
   }
 })();
