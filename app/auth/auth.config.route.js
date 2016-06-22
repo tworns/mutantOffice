fb1cd7d5b0bbd121e5d69c54de373edae811b5b0
tworns <tworns@yahoo.com>
2016-06-22 09:23:13 -0400

    Added routing for registration

---
+++ app/auth/auth.config.route.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+  angular.module('mutantApp.auth').config(configFunc);
+  configFunc.$inject = ['$stateProvider'];
+  function configFunc($stateProvider){
+    $stateProvider.state('register',{
+      url:  '/register',
+      templateUrl: 'app/auth/register.html',
+      controller: 'AuthController',
+      controllerAs: vm
+    });
+  }
+})();
---
+++ app/auth/auth.controller.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+  angular.module('mutantApp.auth').controller('AuthController',AuthController);
+  AuthController.$inject = [];
+  function AuthController() {
+    var vm = this;
+  }
+})();
