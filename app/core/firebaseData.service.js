5d40e5fa0ac7342a6c484f55eac55f34eeff1685
tworns <tworns@yahoo.com>
2016-06-23 09:46:52 -0400

    Added firebaseData.service.js

---
+++ app/core/firebaseData.service.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+  angular.module('mutantApp.core').factory('firebaseDataService', firebaseDataService);
+  
+  function firebaseDataService(){
+    var service = {};
+
+    return service;
+    ////////////////////////////FUNCTIONS BELOW/////////////////////
+  }
+})();
