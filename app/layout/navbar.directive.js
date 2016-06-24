3290f57b78863b33ee351441a0446ae38631232a
tworns <tworns@yahoo.com>
2016-06-24 10:56:14 -0400

    Migrated navbar to its own directive. Turns out Angular gets REALLY angry when it's dashifying custom directives if you camel case them.

---
+++ app/layout/navbar.directive.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+  angular.module('mutantApp.layout').directive('xtNavbar',xtNavbar);
+
+  function xtNavbar(){
+    return {
+      templateUrl: 'app/layout/navbar.html',
+      restrict: 'E',
+    };
+  }
+})();
