f9f4968b730679b8af51d6194aa41d5549ab3e58
tworns <tworns@yahoo.com>
2016-06-24 09:16:48 -0400

    Refactored authController slightly

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,10 +2,9 @@
   'use strict';
   angular.module('mutantApp.auth')
   .controller('AuthController',AuthController);
-  AuthController.$inject = ['$firebaseAuth', '$state', 'authService'];
-  function AuthController($firebaseAuth, $state, authService) {
+  AuthController.$inject = ['$state', 'authService'];
+  function AuthController($state, authService) {
     var vm = this;
-    var auth = $firebaseAuth();
     vm.register = register;
     vm.login = login;
     vm.logout = logout;
