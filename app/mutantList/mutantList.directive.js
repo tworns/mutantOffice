ac2858af9383330b69b43577be05670700e7a357
tworns <tworns@yahoo.com>
2016-06-28 10:11:32 -0400

    Added directive that's still got broken adding mutants

---
+++ app/mutantList/mutantList.directive.js
@@ -0,0 +1,29 @@
+(function(){
+  'use strict';
+  angular.module('mutantApp.mutantList').directive('xtMutantForm', xtMutantForm);
+
+  function xtMutantForm(){
+    return {
+      templateUrl: 'app/mutantList/mutantListForm.html',
+      restrict: 'E',
+      controller: MutantFormController,
+      controllerAs: 'vm',
+      scope: {
+        mutants: '='
+      },
+    };
+  }
+
+  MutantFormController.$inject = ['mutantService', '$scope'];
+  function MutantFormController(mutantService, $scope){
+    var vm = this;
+    //vm.mutants = mutantService.mutantsByUser(user.uid);
+    vm.addMutant = addMutant;
+    vm.newMutant = new mutantService.Mutant();
+
+    function addMutant() {
+      vm.mutants.$add(vm.newMutant);
+      vm.newMutant = new mutantService.Mutant(); //need new here, still calling constructor
+    }
+  }
+})();
