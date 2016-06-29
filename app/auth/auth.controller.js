c1e848926408eb95625a794812d4010e3702d326
tworns <tworns@yahoo.com>
2016-06-29 10:56:23 -0400

    Wrapped login/register functions in a function, learned that JavaScript's comparisons are odd, and everything works now

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -5,35 +5,8 @@
   AuthController.$inject = ['$state', 'authService', 'emailService'];
   function AuthController($state, authService, emailService) {
     var vm = this;
-    vm.register = register;
-    vm.login = login;
     vm.error = null;
-    vm.user = {
-      email: '',
-      password: ''
-    };
-
     ///functions///
-    function register(user){
-      return authService.register(user)
-      .then(function(){
-        vm.login(user);
-      }).then(function(){
-        console.log("USER EMAIL::" +user.email);
-        emailService.sendEmail(user.email);
-      })
-      .catch(function(error){
-        vm.error = error;
-        });
-    }
-
-  function login(user){
-    return authService.login(user)
-    .then(function(loggedInUser){
-      $state.go('mutantList');
-    })
-    .catch(function(error){vm.error = error;});
-  }
 
 }
 })();
