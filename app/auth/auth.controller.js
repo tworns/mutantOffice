8eff5368478817cfa234f4790af3a9dcafb53fac
tworns <tworns@yahoo.com>
2016-06-30 11:09:48 -0400

    Fixed file not found error

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,11 +2,14 @@
   'use strict';
   angular.module('mutantApp.auth')
   .controller('AuthController',AuthController);
-  AuthController.$inject = ['$state', 'authService', 'emailService'];
-  function AuthController($state, authService, emailService) {
+  AuthController.$inject = ['$state', 'authService', 'emailService'];//, '$scope', 'toaster'];
+  function AuthController($state, authService, emailService){//, $scope, toaster) {
     var vm = this;
     vm.error = null;
+    //$scope.pop = pop;
     ///functions///
-
+    // function pop () {
+    //   toaster.pop('succcess',"TEST", "TEST");
+    // }
 }
 })();
